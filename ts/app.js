6abf73e57eca4b1b54d900983c19f94c0087cb62
othmane-boudiab <mrboudiabothmane@gmail.com>
2022-04-12 09:44:18 +0000

    create api

--- ts/app.js
+++ ts/app.js
@@ -1,2 +1,21 @@
-let msg = "Hello World";
-alert(msg);
+// let msg:string = "Hello World";
+// alert(msg);
+function getQuestion() {
+    let myRequest = new XMLHttpRequest();
+    myRequest.onreadystatechange = function () {
+        if (myRequest.readyState == 4 && myRequest.status == 200) {
+            let myData = JSON.parse(myRequest.responseText);
+            console.log(myData);
+        }
+    };
+    myRequest.open("GET", "api/premier.json", true);
+    myRequest.send();
+}
+getQuestion();
+// class Question {
+//     constructor(public question:string, public answer:string) 
+//     {
+//         this.question = question;
+//         this.answer = answer;
+//     }
+// }
--- ts/app.ts
+++ ts/app.ts
@@ -1,2 +1,25 @@
-let msg:string = "Hello World";
-alert(msg);
+// let msg:string = "Hello World";
+// alert(msg);
+
+function getQuestion() {
+    let myRequest:any = new XMLHttpRequest();
+    myRequest.onreadystatechange = function () {
+        if (myRequest.readyState == 4 && myRequest.status == 200) {
+            let myData:string = JSON.parse(myRequest.responseText);
+            console.log(myData);
+            
+        }
+    };
+    myRequest.open("GET", "api/premier.json", true);
+    myRequest.send();
+}
+
+getQuestion();
+
+// class Question {
+//     constructor(public question:string, public answer:string) 
+//     {
+//         this.question = question;
+//         this.answer = answer;
+//     }
+// }
